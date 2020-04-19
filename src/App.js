59f06d49793ada8c4e6c2924418dad7068ead330
Brittany Heller <brittany.heller@live.com>
2020-04-19 12:24:42 -0700

    Add collection items

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shop.component';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
